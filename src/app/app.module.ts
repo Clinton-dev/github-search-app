336e1c67aafb5bd09f75922251b42297562209cc
Clinton wambugu <clintonwambugu@gmail.com>
2022-04-17 16:16:30 +0300

    Create search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepositoryComponent } from './repository/repository.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepositoryComponent
+    RepositoryComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
