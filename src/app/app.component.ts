613ac9ff942814eafe1b7187c37364b9ceb4b915
Clinton wambugu <clintonwambugu@gmail.com>
2022-04-17 14:40:43 +0300

    Add search functionality to reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { GithubService } from './github-service/github.service';
 import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 
 @Component({
@@ -13,23 +14,27 @@ export class AppComponent {
   githubService:GithubService;
   mySubscription: Subscription = new Subscription;
   repositories:any = [];
+  name = new FormControl();
 
   ngOnDestroy():void{
     this.mySubscription.unsubscribe();
   }
 
-  ngOnInit(): void {
-    this.getPublicRepositories()
-  }
+  ngOnInit(): void { }
 
   constructor(githubService:GithubService) {
     this.githubService = githubService;
   }
 
-  getPublicRepositories() {
-     this.mySubscription.add(this.githubService.fetchRepo('Clinton-dev').subscribe((repos:any) => {
-        this.repositories = repos;
-      })
-     )
+  getPublicRepositories(username:string) {
+    this.mySubscription.add(this.githubService.fetchRepo('Clinton-dev').subscribe((repos:any) => {
+      this.repositories = repos;
+    })
+    )
+  }
+
+  searchRepos() {
+    this.getPublicRepositories(this.name.value)
+    return false
   }
 }
